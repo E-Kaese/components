89744e8c8461f429828b8f6476c961f9b571e0e1
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-06-13 11:31:31 +0200

    Fixed experiment pagination page

--- pages/tree-grid/experiment-frame-pagination-configurable.page.tsx
+++ pages/tree-grid/experiment-frame-pagination-configurable.page.tsx
@@ -1,12 +1,12 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { FramePagination, Link } from '~components';
 import styles from './styles.scss';
 import { Instance, generateItems } from '../table/generate-data';
 import clsx from 'clsx';
 import { PageTemplate } from './page-template';
-import { useVirtualScroll } from '~components/tree-grid/use-virtual-scroll';
+import { useVirtualScroll } from '~components/tree-grid/virtual-scroll';
 import { useAppSettings } from '../app/app-context';
 
 const items = generateItems(313);
@@ -45,43 +45,34 @@ export default function App() {
   const frameSize = typeof settings.frameSize === 'number' ? settings.frameSize : defaultPageSettings.frameSize;
   const frameStep = typeof settings.frameStep === 'number' ? settings.frameStep : defaultPageSettings.frameStep;
 
-  const tableHeaderRef = useRef<HTMLTableRowElement>(null);
-  const [headerHeight, setHeaderHeight] = useState(0);
-  useEffect(() => {
-    if (tableHeaderRef.current) {
-      setHeaderHeight(tableHeaderRef.current.getBoundingClientRect().height);
-    }
-  }, []);
-
-  const virtualScroll = useVirtualScroll({ items, frameSize });
-
-  const containerHeight = settings.containerHeight || headerHeight + virtualScroll.scroll.renderedHeight;
+  const containerRef = useRef<HTMLDivElement>(null);
+  const virtualScroll = useVirtualScroll({ size: items.length, frameSize, getContainer: () => containerRef.current });
+  const frameStart = virtualScroll.frame[0];
 
   return (
     <PageTemplate title="Experiment: Virtual scroll with frame pagination">
       {!hidePagination && (
         <FramePagination
           frameSize={frameSize}
-          frameStart={virtualScroll.frame.frameStart}
+          frameStart={frameStart}
           totalItems={items.length}
-          onChange={({ detail }) => virtualScroll.functions.scrollToIndex(detail.frameStart)}
-          onNextPageClick={() => virtualScroll.functions.scrollToIndex(virtualScroll.frame.frameStart + frameStep)}
-          onPreviousPageClick={() => virtualScroll.functions.scrollToIndex(virtualScroll.frame.frameStart - frameStep)}
+          onChange={({ detail }) => virtualScroll.scrollToIndex(detail.frameStart)}
+          onNextPageClick={() => virtualScroll.scrollToIndex(frameStart + frameStep)}
+          onPreviousPageClick={() => virtualScroll.scrollToIndex(frameStart - frameStep)}
         />
       )}
 
       <div
-        ref={virtualScroll.refs.container}
+        ref={containerRef}
         className={styles['custom-table']}
         style={{
-          overflowY: virtualScroll.scrollable ? 'auto' : 'unset',
-          height: virtualScroll.scrollable ? containerHeight : 'unset',
+          overflowY: 'auto',
+          height: '800px',
         }}
-        onScroll={event => virtualScroll.handlers.onScroll((event.target as HTMLElement).scrollTop)}
       >
         <table className={styles['custom-table-table']}>
           <thead>
-            <tr ref={tableHeaderRef}>
+            <tr>
               {columnDefinitions.map(column => (
                 <th key={column.key} className={clsx(styles['custom-table-cell'], styles['custom-table-header'])}>
                   {column.label}
@@ -91,33 +82,34 @@ export default function App() {
           </thead>
 
           <tbody>
-            {virtualScroll.scroll.heightBefore > 0 ? (
-              <tr>
-                <td
-                  colSpan={columnDefinitions.length}
-                  style={{ padding: 0, margin: 0, height: virtualScroll.scroll.heightBefore }}
-                />
-              </tr>
-            ) : null}
+            <tr>
+              <td
+                ref={virtualScroll.elBeforeRef}
+                colSpan={columnDefinitions.length}
+                style={{ padding: 0, margin: 0, height: 0 }}
+              />
+            </tr>
 
-            {virtualScroll.frame.items.map(({ item, index }) => (
-              <tr key={item.id} ref={virtualScroll.refs.row.bind(null, index)}>
-                {columnDefinitions.map(column => (
-                  <td key={column.key} className={styles['custom-table-cell']}>
-                    {column.render(item)}
-                  </td>
-                ))}
-              </tr>
-            ))}
+            {virtualScroll.frame.map(index => {
+              const item = items[index];
+              return (
+                <tr key={item.id} ref={virtualScroll.setItemRef.bind(null, index)}>
+                  {columnDefinitions.map(column => (
+                    <td key={column.key} className={styles['custom-table-cell']}>
+                      {column.render(item)}
+                    </td>
+                  ))}
+                </tr>
+              );
+            })}
 
-            {virtualScroll.scroll.heightAfter > 0 ? (
-              <tr>
-                <td
-                  colSpan={columnDefinitions.length}
-                  style={{ padding: 0, margin: 0, height: virtualScroll.scroll.heightAfter }}
-                />
-              </tr>
-            ) : null}
+            <tr>
+              <td
+                ref={virtualScroll.elAfterRef}
+                colSpan={columnDefinitions.length}
+                style={{ padding: 0, margin: 0, height: 0 }}
+              />
+            </tr>
           </tbody>
         </table>
       </div>
