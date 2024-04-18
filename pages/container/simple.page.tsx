92da606b367444e49ea3770bd973d9b464790b93
Scott O'Brien <scottob@amazon.com>
2024-04-18 12:20:15 -0400

    Test pages with new styles.

--- pages/container/simple.page.tsx
+++ pages/container/simple.page.tsx
@@ -3,17 +3,34 @@
 import React from 'react';
 import Container from '~components/container';
 import ScreenshotArea from '../utils/screenshot-area';
+import { SpaceBetween } from '~components';
 
 export default function SimpleContainers() {
   return (
     <article>
       <h1>Simple containers</h1>
       <ScreenshotArea>
-        <div>
-          <Container variant="stacked"> Content</Container>
-          <Container variant="stacked"> Content</Container>
-          <div></div>
-        </div>
+        <SpaceBetween direction="vertical" size="m">
+          <div>
+            <Container variant="stacked">first container</Container>
+            <Container variant="stacked">last container, with trailing div</Container>
+            <div></div>
+          </div>
+
+          <div>
+            <Container variant="stacked">first container</Container>
+            <Container variant="stacked">last container, no trailing div</Container>
+          </div>
+
+          <div>
+            <div></div>
+            <Container variant="stacked">first container, with leading div</Container>
+            <Container variant="stacked">container</Container>
+            <Container variant="stacked">container</Container>
+            <Container variant="stacked">last container, with trailing div</Container>
+            <div></div>
+          </div>
+        </SpaceBetween>
       </ScreenshotArea>
     </article>
   );
