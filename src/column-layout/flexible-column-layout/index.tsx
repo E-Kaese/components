03d055ad6579191680657ee12b3a2dbd5face242
Scott O'Brien <scottob@amazon.com>
2024-05-16 21:59:51 -0400

    POC replacement of container query hook.

--- src/column-layout/flexible-column-layout/index.tsx
+++ src/column-layout/flexible-column-layout/index.tsx
@@ -5,28 +5,6 @@ import clsx from 'clsx';
 import flattenChildren from 'react-keyed-flatten-children';
 import { InternalColumnLayoutProps } from '../interfaces';
 import styles from './styles.css.js';
-import { useContainerQuery } from '@cloudscape-design/component-toolkit';
-
-const isOdd = (value: number): boolean => value % 2 !== 0;
-
-export function calculcateCssColumnCount(
-  columns: number,
-  minColumnWidth: number,
-  containerWidth: number | null
-): number {
-  if (!containerWidth) {
-    return columns;
-  }
-
-  // First, calculate how many columns we can have based on the current container width and minColumnWidth.
-  const targetColumnCount = Math.min(columns, Math.floor(containerWidth / minColumnWidth));
-
-  // When we start wrapping into fewer columns than desired, we want to keep the number of columns even.
-  return Math.max(
-    1,
-    targetColumnCount < columns && isOdd(targetColumnCount) ? targetColumnCount - 1 : targetColumnCount
-  );
-}
 
 interface FlexibleColumnLayoutProps
   extends Pick<InternalColumnLayoutProps, 'minColumnWidth' | 'columns' | 'variant' | 'borders' | 'disableGutters'> {
@@ -40,9 +18,6 @@ export default function FlexibleColumnLayout({
   variant,
   children,
 }: FlexibleColumnLayoutProps) {
-  const [containerWidth, containerRef] = useContainerQuery(rect => rect.contentBoxWidth);
-
-  const columnCount = calculcateCssColumnCount(columns, minColumnWidth, containerWidth);
   const shouldDisableGutters = variant !== 'text-grid' && disableGutters;
 
   // Flattening the children allows us to "see through" React Fragments and nested arrays.
@@ -50,13 +25,13 @@ export default function FlexibleColumnLayout({
 
   return (
     <div
-      ref={containerRef}
       className={clsx(
         styles['css-grid'],
         styles[`grid-variant-${variant}`],
         shouldDisableGutters && [styles['grid-no-gutters']]
       )}
-      style={{ gridTemplateColumns: `repeat(${columnCount}, 1fr)` }}
+      style={{ '--min-column-width': `${minColumnWidth}px`, '--column-count': columns }}
+      // gridTemplateColumns: `repeat(${columns}, 1fr)`
     >
       {flattenedChildren.map((child, i) => {
         // If this react child is a primitive value, the key will be undefined
@@ -65,10 +40,7 @@ export default function FlexibleColumnLayout({
         return (
           <div
             key={key ? String(key) : undefined}
-            className={clsx(styles.item, {
-              [styles['first-column']]: i % columnCount === 0,
-            })}
-          >
+            className={clsx(styles.item)}>
             {child}
           </div>
         );
